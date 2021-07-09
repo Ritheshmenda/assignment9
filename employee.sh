1a8986cffe3b1c9958882c184207cc1fae4939fb
Ritheshmenda <ritheshmenda123@gmail.com>
2021-07-09 22:51:03 +0530

    added: uc7-totalfunction

--- employee.sh
+++ employee.sh
@@ -1,28 +1,32 @@
 #!/bin/bash -x
-echo "employee wage computation program on master branch"
-ispartime=1
-isfulltime=2
-empwageprhr=20
-ft_hours=10
+fulltime=1
+partime=2
+empwageperhr=20
 days=20
-minhr=0
+maxhrs=100
+minhrs=0
 mindays=0
-while [[ $minhrs -lt $ft_hours && $mindays -lt $days ]]
-do
-((mindays++))
-emp=$((RANDOM%3))
-case $emp in
- $isfulltime)
- emphrs=8
+function workinghrs
+{
+case "$1" in
+$fulltime)
+emphrs=8
 ;;
- $ispartime)
- emphrs=6
+$partime)
+emphrs=8
 ;;
 *)
- emphrs=0
+emphrs=0
 ;;
 esac
+echo $emphrs
+}
+while [[ $minhrs -lt $maxhrs && $mindays -lt $days ]]
+do
+((mindays++))
+emphrs="$( workinghrs $((RANDOM%3)) )"
 minhrs=$(($minhrs+$emphrs))
 done
-totalsalary=$(($minhrs*$empwageprhr))
+totalsalary=$(($minhrs*$empwageperhr))
+
 
