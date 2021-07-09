8f7a140d8c3360f5a09d3a103ce8a7bcf271b110
Ritheshmenda <ritheshmenda123@gmail.com>
2021-07-09 20:19:35 +0530

    added: uc5-monthlywage

--- employee.sh
+++ employee.sh
@@ -2,12 +2,13 @@ echo "employee wage computation program on master branch"
 empwage=20
 ft=10
 pt=8
+days=20
 emp=$((RANDOM%3))
 case "$emp" in
-1) (( wage = $empwage*$ft ))
+1) (( wage = ($empwage*$ft) * $days ))
 echo "fulltime wage is $wage"
 ;;
-2) (( wage = $empwage*$pt ))
+2) (( wage = ($empwage*$pt) * $days ))
 echo "parttime wage is $wage"
 ;;
 *) echo "absent"
