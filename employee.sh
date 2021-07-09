2c4a5aa5c471fada3642dcb0fb88c6c714173655
Ritheshmenda <ritheshmenda123@gmail.com>
2021-07-09 18:02:42 +0530

    added: uc2-dailywage

--- employee.sh
+++ employee.sh
@@ -1,10 +1,16 @@
 echo "employee wage computation program on master branch"
 ispresent=1
+totalsalary=0
+emprateperhour=20
+numberofworkinghr=8
 attendance=$((RANDOM%2))
 if [ $ispresent -eq $attendance ]
 then
+salary=$(($emprateperhour*$numberofworkinghr))
+echo "salary=$salary"
 echo "employee is presnt"
 else
 echo "employee is absent"
+echo "no pay"
 fi
 
