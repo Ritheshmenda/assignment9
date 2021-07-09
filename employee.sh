66dc3daee540e228d9c49fa76ba5e22aa4f4e701
Ritheshmenda <ritheshmenda123@gmail.com>
2021-07-09 17:49:41 +0530

    added: uc1-empattendance

--- employee.sh
+++ employee.sh
@@ -1,1 +1,10 @@
 echo "employee wage computation program on master branch"
+ispresent=1
+attendance=$((RANDOM%2))
+if [ $ispresent -eq $attendance ]
+then
+echo "employee is presnt"
+else
+echo "employee is absent"
+fi
+
