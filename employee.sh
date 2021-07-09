82ea612facd9dd65a316298dfb9beadb84631397
Ritheshmenda <ritheshmenda123@gmail.com>
2021-07-09 23:14:14 +0530

    added: uc8-dailywagetotal

--- employee.sh
+++ employee.sh
@@ -6,11 +6,12 @@ days=20
 maxhrs=100
 minhrs=0
 mindays=0
+totalemphrs=0
 function workinghrs
 {
 case "$1" in
 $fulltime)
-emphrs=8
+emphrs=10
 ;;
 $partime)
 emphrs=8
@@ -21,12 +22,17 @@ emphrs=0
 esac
 echo $emphrs
 }
+function dwcal
+{
+wage=$(($emphrs*$empwageperhr))
+echo $wage
+}
 while [[ $minhrs -lt $maxhrs && $mindays -lt $days ]]
 do
 ((mindays++))
 emphrs="$( workinghrs $((RANDOM%3)) )"
 minhrs=$(($minhrs+$emphrs))
+empdailywage[$mindays]="$( dwcal $emphrs)"
 done
-totalsalary=$(($minhrs*$empwageperhr))
-
-
+totalsalary="$( dwcal $totalemphrs )"
+echo "daily wage" ${empdailywage[@]}
